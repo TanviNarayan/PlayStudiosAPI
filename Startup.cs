ea25bbb08d729d2e3f8634957729128a77e8aa23
TanviNarayan <32529189+TanviNarayan@users.noreply.github.com>
2020-10-26 12:28:49 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlayStudiosApi.Interfaces;
+using PlayStudiosApi.Data;
+using PlayStudios.Model;
+
+namespace PlayStudiosApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddMvc();
+            
+            services.Configure<Settings>(options =>
+            {
+                options.ConnectionString = "mongodb://palas:p12345@ds030719.mlab.com:30719/playstudio?retryWrites=false";
+                options.Database = "playstudio";
+            });
+
+            services.AddTransient<IUserRepository, UserRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors("CorsPolicy");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
